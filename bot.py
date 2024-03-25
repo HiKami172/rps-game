fe34db757beda0c7ff30a2eacc3c34369f88f561
hikami <hikami172@gmail.com>
2024-03-25 05:47:36 +0100

    Add stable distribution calculation and initial state processing.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 This module implements a bot model for playing the Rock-Paper-Scissors game.
 The model is designed as a class `RPSBot` which uses a Markov chain for prediction.
 """
-from typing import List
+from typing import List, Optional
 import numpy as np
 
 
@@ -16,6 +16,14 @@ class RPSBot:
         self.lr = lr
         self.transitions = np.random.rand(3, 3)
         self.transitions /= self.transitions.sum(axis=1)[:, np.newaxis]
+
+    def _calculate_stable_distribution(self) -> np.ndarray:
+        """Calculate the stable distribution of the transitions' matrix."""
+        eigenvalues, eigenvectors = np.linalg.eig(self.transitions.T)
+        first_eigen = eigenvectors[:, np.where(np.abs(eigenvalues - 1.0) < 1e-8)[0][0]]
+        stable_distribution = np.real(first_eigen)
+        stable_distribution /= stable_distribution.sum()
+        return stable_distribution
 
     def train(self, history: List[str]) -> None:
         """Trains the model based on the states history."""
@@ -31,8 +39,12 @@ class RPSBot:
         self.transitions[state_index] -= self.lr
         self.transitions[state_index, new_state_index] += self.lr * 3
 
-    def predict(self, state: str) -> str:
+    def predict(self, state: Optional[str] = None) -> str:
         """Predicts the next state based on current and returns counter move."""
+        if state is None:
+            stable_distribution = self._calculate_stable_distribution()
+            response_index = np.argmax(stable_distribution)
+            return self.states[response_index]
         if state not in self.states:
             raise ValueError("Invalid state")
         state_index = self.states.index(state)
